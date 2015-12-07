efc16901270e33890999ef9642bb3706f0e342cd
jeromechan <jerome.chan369@gmail.com>
2015-12-07 20:50:11 +0800

    第一个版本
    第一个版本

---
+++ src/main/java/mapper/GuideBasicMapper.java
@@ -0,0 +1,7 @@
+package main.java.mapper;
+
+import main.java.model.GuideBasic;
+
+public interface GuideBasicMapper {
+    int deleteByPrimaryKey(Integer id);
+}
---
+++ src/main/java/model/GuideBasic.java
@@ -0,0 +1,25 @@
+package main.java.model;
+
+import java.util.Date;
+
+public class GuideBasic {
+    private Integer id;
+
+    private Byte name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Byte getName() {
+        return name;
+    }
+
+    public void setName(Byte name) {
+        this.name = name;
+    }
+}
